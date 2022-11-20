86d31468325173091b1e6ea06784a9b658f11a3e
muink <hukk1996@gmail.com>
2022-11-20 18:13:09 +0800

    manually start

--- htdocs/luci-static/resources/view/netdata/config.js
+++ htdocs/luci-static/resources/view/netdata/config.js
@@ -30,6 +30,25 @@ return view.extend({
 		o.placeholder = '19999';
 		o.rmempty = false;
 
+		o = s.option(form.Flag, 'logger', _('Enable logger'));
+		o.rmempty = true;
+
+		o = s.option(form.Button, '_start', _('Start ') + _('NetData'));
+		o.inputtitle = _('Start');
+		o.inputstyle = 'apply';
+		o.onclick = function() {
+			return fs.exec('/etc/init.d/netdata', ['start'])
+				.catch(function(e) { ui.addNotification(null, E('p', e.message), 'error') });
+		};
+
+		o = s.option(form.Button, '_stop', _('Stop ') + _('NetData'));
+		o.inputtitle = _('Stop');
+		o.inputstyle = 'reset';
+		o.onclick = function() {
+			return fs.exec('/etc/init.d/netdata', ['stop'])
+				.catch(function(e) { ui.addNotification(null, E('p', e.message), 'error') });
+		};
+
 		return m.render();
 	}
 });
